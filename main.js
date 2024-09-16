8ebadf531bcad0b7172987ed4daf20b2ce6ef7cd
prinsergioprin <sergioprin24@gmail.com>
2024-09-16 13:23:58 +0200

    Set up hover effect so that the grid divs change colour

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-let board = document.querySelector('.board')
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
+function populateBoard(size) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for(let i=0; i<256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
-    board.insertAdjacentElement('beforeend', square);
+    let amount = size * size
+    for(let i=0; i<amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+        square.style.backgroundColor = "blue";
+        board.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if(input >=2 || input <=100) {
+        populateBoard(input);
+    } else {
+        console.log("Too many squares");
+    }
+    
 }
