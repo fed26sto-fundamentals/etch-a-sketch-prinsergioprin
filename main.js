1ea6d3467c7d45e6e0606fa902e40ed06381e6d0
prinsergioprin <sergioprin24@gmail.com>
2024-09-16 14:24:57 +0200

    Added button to reset board

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let color = "black";
+let click = true;
+
 function populateBoard(size) {
-    let board = document.querySelector(".board")
+    let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -8,10 +11,8 @@ function populateBoard(size) {
     let amount = size * size
     for(let i=0; i<amount; i++) {
         let square = document.createElement("div");
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
-        });
-        square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", colorSquare); 
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -19,10 +20,41 @@ function populateBoard(size) {
 populateBoard(16);
 
 function changeSize(input) {
-    if(input >=2 || input <=100) {
+    if(input >=2 && input <=100) {
+        document.querySelector(".error").style.display = "none";
         populateBoard(input);
     } else {
-        console.log("Too many squares");
-    }
-    
+        document.querySelector(".error").style.display = "flex";
+    }   
 }
+
+function colorSquare() {
+    if(click) {
+        if(color === "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if(e.target.tagName != "BUTTON") {
+        click = !click;
+        if(click) {
+            document.querySelector(".mode").textContent = "Mode: Colouring";
+        } else {
+            document.querySelector(".mode").textContent = "Mode: Not Colouring";
+        }
+    }   
+});
